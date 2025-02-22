f3e4929b2ce783166fa2d5315d2d9499f9bc00db
SabinZibileanu <s_zibileanu@yahoo.com>
2025-02-22 10:31:14 +0200

    added max number of k sum pairs problem

---
+++ LeetCode-75-Python/Max_Number_of_K_Sum_Pairs.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        left_pointer = 0
+        right_pointer = len(nums) - 1
+        nr_possible_divisions = len(nums) // 2
+
+        sorted_array = sorted(nums)
+        counter_operations = 0
+
+        while (left_pointer < right_pointer) and (counter_operations < nr_possible_divisions):
+            print(left_pointer, right_pointer)
+            if sorted_array[left_pointer] + sorted_array[right_pointer] < k:
+                left_pointer += 1
+            
+            elif sorted_array[left_pointer] + sorted_array[right_pointer] > k:
+                right_pointer -= 1
+            
+            else:
+                counter_operations += 1
+                left_pointer += 1
+                right_pointer -= 1
+        
+        return counter_operations
+
+
+s = Solution()
+print(s.maxOperations([3,5,1,5], 2))
